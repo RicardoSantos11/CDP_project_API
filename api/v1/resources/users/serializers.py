5c1f09d3652d13d57214db8234706f5df7794407
RicardoTurco <ricturco@gmail.com>
2020-07-08 00:04:47 -0300

    Update serializer of User

--- api/v1/resources/users/serializers.py
+++ api/v1/resources/users/serializers.py
@@ -6,9 +6,12 @@ user = api.model('User', {
     '_id': fields.String(readonly=True, description='User ID'),
     'name': fields.String(required=True, description='Name of User'),
     'email': fields.String(required=True, description='Email of User'),
-    'phone': fields.String(required=True, description='Phone of User'),
+    'mobile_phone': fields.String(required=True, description='Phone of User'),
     'cpf': fields.String(required=True, description='CPF of User'),
-    'birth_date': fields.String(required=True, description='Birth Date of User'),
+    'birth_date': fields.Date(required=True, description='Birth Date of User'),
     'brand': fields.String(required=True, description='Brand of User'),
-    'source': fields.String(required=True, description='Source of User')
+    'source': fields.String(required=True, description='Source of User'),
+    'privacy_consent': fields.String(required=True, description='privacy_consent of User'),
+    'media_consent': fields.String(required=True, description='media_consent of User'),
+    'reg_date': fields.String(required=True, description='reg_date of User')
 })
